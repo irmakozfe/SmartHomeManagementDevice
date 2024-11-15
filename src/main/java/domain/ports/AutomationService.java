4918a39d1a7a5906f3f7641c1750c4b8130fbcec
Irmak Damla Özdemir <123468140+irmakozfe@users.noreply.github.com>
2024-11-15 20:10:55 +0100

    Update AutomationService.java

--- src/main/java/domain/ports/AutomationService.java
+++ src/main/java/domain/ports/AutomationService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AutomationService {
+    
     AutomationRule createAutomationRule(AutomationRule rule);
     AutomationRule updateAutomationRule(int id, AutomationRule newRule);
     void deleteAutomationRule(int id);
